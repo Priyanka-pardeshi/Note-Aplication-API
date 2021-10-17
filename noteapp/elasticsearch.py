a577ee8fcb8c685c291d05aa4ad9f4ab4d1d38aa
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-10-17 15:41:40 +0530

    Applied elastic search for Get, Post, Delete

--- noteapp/elasticsearch.py
+++ noteapp/elasticsearch.py
@@ -10,28 +10,32 @@ class Elastic_search:
     def __init__(self):
         self.es = Elasticsearch(hosts=settings.ES_HOST, port=settings.ES_PORT)
 
-    def post_data(self):
-        note={"title":"notes", "description": "hii", "user_id": 2, "id": 1}
-        es_data = self.es.index(index="note", id=1, body=note)
+    def post_data(self, note_data):
+        es_data = self.es.index(index="note", id=1, body=note_data)
         return es_data['result']
 
-    def get_data(self):
+    def get_data(self, user_id):
         query = {
             "query": {
                 "term": {
-                    "title": "elastic notes"
+                    "user_id": user_id
                 }
             }
         }
         data = self.es.search(index="note", body=query)
-        return data['hits']['hits']
+        list_data = data['hits']['hits']
+        note_list = []
+        for data in list_data:
+            print(data.get("_source"), " :data")
+            note_list.append(data.get("_source"))
+        return note_list
 
-    def delete_data(self):
+    def delete_data(self, note_name):
         query = {
             "query": {
                 "match": {
-                    "id": "1"
+                    "title": note_name
                 }
             }
         }
-        self.es.delete(index="my_elastic_search", body=query)
+        self.es.delete_by_query(index="note", body=query)
